798ac3e1df107e9368cb2adea61b1a045f1d1e00
Momo <seckndanane16@gmail.com>
2022-04-12 01:55:57 +0200

    Lesson 13: Form validation avec message d'erreur sous forme de balise li tag

--- app/Http/Controllers/HtmlFormController.php
+++ app/Http/Controllers/HtmlFormController.php
@@ -7,7 +7,16 @@ use Illuminate\Http\Request;
 class HtmlFormController extends Controller
 {
     //
+
     function getData(Request $req) {
+
+        $req -> validate([
+            'username' => 'required',
+            'password' =>'required'
+        ]);
+
+
+
         return $req->input();
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,11 @@
 <h1>Login page</h1>
-
+<!-- VERSION DE VALIDATION AVEC MESSAGE D'ERREUR SOUS FORM DE <LI>
+    @if($errors->any())
+    @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+    @endforeach
+@endif
+-->
 <form action="login" method="POST">
     @csrf
     <input type="text" name="username" placeholder="Enter user name"><br>
